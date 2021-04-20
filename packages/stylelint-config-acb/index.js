f9364c592427c8e98a7eb701f8162178c4c7ff19
tomislav-s <tomislav@acolorbright.com>
2021-04-20 11:00:10 +0200

    chore: adjust stylelint-config-acb formatting

--- packages/stylelint-config-acb/index.js
+++ packages/stylelint-config-acb/index.js
@@ -1,16 +1,16 @@
 module.exports = {
   extends: [
-    "stylelint-config-recommended",
-    "stylelint-config-property-sort-order-acb",
+    'stylelint-config-recommended',
+    'stylelint-config-property-sort-order-acb',
   ],
   rules: {
-    "at-rule-no-unknown": [true, { "ignoreAtRules": ["define-mixin", "mixin"]}],
-    "at-rule-no-vendor-prefix": true,
-    "color-hex-case": "lower",
-    "color-hex-length": "short",
-    "color-no-invalid-hex": true,
-    "max-nesting-depth": 3,
-    "media-feature-name-no-vendor-prefix": true,
-    "value-no-vendor-prefix": true,
-  }
-}
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['define-mixin', 'mixin'] }],
+    'at-rule-no-vendor-prefix': true,
+    'color-hex-case': 'lower',
+    'color-hex-length': 'short',
+    'color-no-invalid-hex': true,
+    'max-nesting-depth': 3,
+    'media-feature-name-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+  },
+};
