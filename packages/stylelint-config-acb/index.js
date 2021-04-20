58b9b7add39f6b9bbc491543cf3868ef6db2d10a
tomislav-s <tomislav@acolorbright.com>
2021-04-20 15:12:51 +0200

    chore: extend stylelint config

--- packages/stylelint-config-acb/index.js
+++ packages/stylelint-config-acb/index.js
@@ -9,8 +9,18 @@ module.exports = {
     'color-hex-case': 'lower',
     'color-hex-length': 'short',
     'color-no-invalid-hex': true,
+    'length-zero-no-unit': true,
     'max-nesting-depth': 3,
+    'media-feature-name-disallowed-list': ['max-width'],
     'media-feature-name-no-vendor-prefix': true,
+    'media-feature-name-value-allowed-list': {
+      'min-width': ['/^[0-9]*em$/'],
+    },
+    'number-leading-zero': 'always',
+    'number-no-trailing-zeros': true,
+    'unit-case': 'lower',
+    'value-list-comma-space-after': 'always-single-line',
+    'value-list-comma-space-before': 'never-single-line',
     'value-no-vendor-prefix': true,
   },
 };
