b7568d060d85a181dc0009139dba38ec68e70d91
tomislav-s <tomislav@acolorbright.com>
2021-05-12 16:02:48 +0200

    chore: remove obsolete file

--- packages/eslint-config-ts-react/rules/typescript.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'no-use-before-define': 'off',
-  },
-}
