b9bef96e738b4ff57925ddefec62a0c4a6a86a17
tomislav-s <tomislav@acolorbright.com>
2022-06-20 10:01:41 +0200

    feat: forbid img elements without width and height attributes

--- packages/eslint-config/rules/extras.js
+++ packages/eslint-config/rules/extras.js
@@ -1,6 +1,8 @@
 module.exports = {
-	plugins: ['extra-rules'],
+	plugins: ['extra-rules', '@mizdra/layout-shift'],
 	rules: {
 		'extra-rules/no-commented-out-code': 'error',
+		// Forbid img element without explicit size attributes
+		'@mizdra/layout-shift/require-size-attributes': 'error',
 	},
 };
